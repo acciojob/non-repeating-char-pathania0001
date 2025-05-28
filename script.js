2270bf911b7cfefc19074725590111c6cdb64748
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-05-28 10:07:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  let map = set Map();
+	let size = str.length;
+	for(let i=0;i<size;i++)
+		map.set( str[i] , ( map.get(str[i] ) || 0) + 1 );
+	
+	for(let i=0;i<size;i++)
+		{
+			if(map.get(str[i]) ==  1)
+				return str[i];
+			
+		}
+	return null;
 }
-const input = prompt("Enter a string");
+ const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
